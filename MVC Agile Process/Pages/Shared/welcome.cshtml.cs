20569d47a95846552b0c811e4e93a79cd7a246e9
Alex Xiong <gxiong1@my.centennialcollege.ca>
2024-03-15 20:56:25 -0400

    Complete Code

---
+++ MVC Agile Process/Pages/Shared/welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVC_Agile_Process.Pages.Shared
+{
+    public class welcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MVC Agile Process/Program.cs
+++ MVC Agile Process/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_Agile_Process.Data;
+using MVC_Agile_Process.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MVC_Agile_ProcessContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVC_Agile_ProcessContext") ?? throw new InvalidOperationException("Connection string 'MVC_Agile_ProcessContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +25,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
